902a80e7c4f1c7ee4b4891fdad3c801593ea3543
admin <admin>
2024-05-07 15:16:32 -0400

    Edited Library file "python/logging/log.py"

--- lib/python/logging/log.py
+++ lib/python/logging/log.py
@@ -1,9 +1,5 @@
 import logging
-import mylib
 logger = logging.getLogger(__name__)
 
-def main():
-    logging.basicConfig(filename='myapp.log', level=logging.INFO)
-    logger.info('Started')
-    mylib.do_something()
-    logger.info('Finished')
+def do_something():
+    logger.info('Doing something')
