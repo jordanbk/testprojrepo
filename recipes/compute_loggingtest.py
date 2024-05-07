7f7ed5fbfc59b03de446597639525426c0ff58b1
admin <admin>
2024-05-07 18:24:52 -0400

    Saved recipe: TEST.compute_loggingtest

--- recipes/compute_loggingtest.py
+++ recipes/compute_loggingtest.py
@@ -2,40 +2,13 @@
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
-from lgg import mylib
-import logging
-logging.basicConfig(filename='/Users/jordanburke/Library/DataScienceStudio/dss_home/tmp/myapp.log', level=logging.INFO, format='%(asctime)s %(message)s', force=True)
 import logging
 
-# create logger with 'spam_application'
-logger = logging.getLogger('spam_application')
+logger = logging.getLogger('simple_example')
 logger.setLevel(logging.DEBUG)
 # create file handler which logs even debug messages
-fh = logging.FileHandler('/Users/jordanburke/Library/DataScienceStudio/dss_home/tmp/spam.log')
+fh = logging.FileHandler('/Users/jordanburke/Library/DataScienceStudio/dss_home/tmp/myapp.log')
 fh.setLevel(logging.DEBUG)
-# create console handler with a higher log level
-ch = logging.StreamHandler()
-ch.setLevel(logging.ERROR)
-# create formatter and add it to the handlers
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-fh.setFormatter(formatter)
-ch.setFormatter(formatter)
-# add the handlers to the logger
-logger.addHandler(fh)
-logger.addHandler(ch)
-
-logger.info('creating an instance of auxiliary_module.Auxiliary')
-a = mylib.Auxiliary()
-logger.info('created an instance of auxiliary_module.Auxiliary')
-logger.info('calling auxiliary_module.Auxiliary.do_something')
-
-logger.info('finished auxiliary_module.Auxiliary.do_something')
-logger.info('calling auxiliary_module.some_function()')
-
-logger.info('done with auxiliary_module.some_function()')
-
-logger.info('Started')
-
 
 logger.info('Started')
 data = [['tom', 10], ['nick', 15], ['juli', 14]]
