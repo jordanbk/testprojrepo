cc21dfbe304b701c0cb5169497c68ed03b63dc5c
admin <admin>
2024-05-07 15:12:32 -0400

    Saved recipe: TEST.compute_loggingtest

--- recipes/compute_loggingtest.py
+++ recipes/compute_loggingtest.py
@@ -2,12 +2,13 @@
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
-
+import log from logging
 import logging
+
 logger = logging.getLogger(__name__)
-logging.basicConfig(filename="/Users/jordanburke/Library/DataScienceStudio/dss_home/loggingtest.log",
-                format='%(asctime)s %(message)s',
-                filemode='w')
+
+def do_something():
+    logger.info('Doing something')
 
 logger.info('Started')
 data = [['tom', 10], ['nick', 15], ['juli', 14]]
@@ -18,8 +19,7 @@ df = pd.DataFrame(data, columns=['Name', 'Age'])
 
 loggingtest_df = df
 logger.info('Finished')
-if __name__ == '__main__':
-    main()
+
 
 # Write recipe outputs
 loggingtest = dataiku.Dataset("loggingtest")
