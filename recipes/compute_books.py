f0f5cd06215032a9782bc59e3665f3f287f80c61
admin <admin>
2024-06-17 13:17:41 -0400

    Saved recipe: TEST.compute_books

---
+++ recipes/compute_books.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import dataiku
+import pandas as pd, numpy as np
+from dataiku import pandasutils as pdu
+
+# Read recipe inputs
+XML = dataiku.Folder("16soUqEn")
+XML_info = XML.get_info()
+
+paths = handle.list_paths_in_partition()
+
+with handle.get_download_stream("books.xml") as f:
+    data = pd.read_xml(f)
+
+books_df = data
+
+
+# Write recipe outputs
+books = dataiku.Dataset("books")
+books.write_with_schema(books_df)
