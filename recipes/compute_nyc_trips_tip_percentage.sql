198268bcd5f15c8a60c8b1f80ad3d36526f061da
admin <admin>
2024-10-03 12:24:07 -0400

    Saved recipe: TEST.compute_nyc_trips_tip_percentage

--- recipes/compute_nyc_trips_tip_percentage.sql
+++ recipes/compute_nyc_trips_tip_percentage.sql
@@ -2,7 +2,7 @@ SELECT
     *,
     CASE 
         WHEN total_amount = 0 THEN 0
-        ELSE (tip_amount / total_amount) * 100
+        ELSE (tip_amount::float / total_amount::float) * 100
     END AS tip_percentage
 FROM 
     "public"."SQL_nyc_trips_copy"
