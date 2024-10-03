772ad23df7d4debd9ded39d3a54f910cf1bdae4f
admin <admin>
2024-10-03 12:23:05 -0400

    Saved recipe: TEST.compute_nyc_trips_tip_percentage

--- recipes/compute_nyc_trips_tip_percentage.sql
+++ recipes/compute_nyc_trips_tip_percentage.sql
@@ -1,5 +1,5 @@
 SELECT 
     *,
-    (tip_amount::float / total_amount::float) * 100 AS tip_percentage::float
+    (tip_amount::float / total_amount::float) * 100 AS tip_percentage
 FROM 
     "public"."SQL_nyc_trips_copy"
